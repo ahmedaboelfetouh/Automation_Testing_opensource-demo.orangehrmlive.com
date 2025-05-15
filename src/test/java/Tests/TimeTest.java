d617471cb0781b9a4c491a4b82db42c3863cca0b
ahmedaboelfetouh <am614663@gmail.com>
2025-05-15 20:01:46 +0300

    Coomits > Update > LogInOutPage

--- src/test/java/Tests/TimeTest.java
+++ src/test/java/Tests/TimeTest.java
@@ -8,26 +8,6 @@ import org.testng.annotations.*;
 
 public class TimeTest extends BaseTest {
 
-    @Test(dependsOnMethods = "PunchInTest")
-    public void PunchOutTest(){
-        logger.info("Time Module ➡\uFE0F \uD83D\uDE80\uD83D\uDE80\uD83D\uDE80 Starting Punch Out Test in Time Module");
-        TimePage time = new TimePage(driver);
-        time.OpenTimeModule();
-        time.OpenAttendanceMenu();
-        time.OpenPunchInOutModule();
-        time.SetPunchOutData();
-        time.SubmitPunchOut();
-        logger.debug("Time module ➡\uFE0F ✅✅✅ Punch out Test Completed Successfully");
-    }
-
-    @AfterMethod
-    public void Logout(){
-        logger.info("Time Module ➡\uFE0F \uD83D\uDE80\uD83D\uDE80\uD83D\uDE80 Login Out");
-        LogInOutPage login = new LogInOutPage(driver);
-        login.Logout();
-        logger.debug("Time module ➡\uFE0F ✅✅✅ Logout Completed");
-    }
-
     @BeforeMethod
     public void Login(){
         logger.info("Time Module ➡\uFE0F \uD83D\uDE80\uD83D\uDE80\uD83D\uDE80 Start Login");
@@ -46,5 +26,25 @@ public class TimeTest extends BaseTest {
         time.SetPunchInData();
         time.SubmitPunchIn();
         logger.debug("Time module ➡\uFE0F ✅✅✅ Punch In Test Completed Successfully");
+    }
+
+    @Test
+    public void PunchOutTest(){
+        logger.info("Time Module ➡\uFE0F \uD83D\uDE80\uD83D\uDE80\uD83D\uDE80 Starting Punch Out Test in Time Module");
+        TimePage time = new TimePage(driver);
+        time.OpenTimeModule();
+        time.OpenAttendanceMenu();
+        time.OpenPunchInOutModule();
+        time.SetPunchOutData();
+        time.SubmitPunchOut();
+        logger.debug("Time module ➡\uFE0F ✅✅✅ Punch out Test Completed Successfully");
+    }
+
+    @AfterMethod
+    public void Logout(){
+        logger.info("Time Module ➡\uFE0F \uD83D\uDE80\uD83D\uDE80\uD83D\uDE80 Login Out");
+        LogInOutPage login = new LogInOutPage(driver);
+        login.Logout();
+        logger.debug("Time module ➡\uFE0F ✅✅✅ Logout Completed");
     }
 }
