1123748d394e74bb1c5b6017c408e16f677f4d9b
NarimanFarag <nariman.farag454@gmail.com>
2025-05-17 12:52:15 +0300

    Update(RecruitmentPage,MyRecruitmentPageTest,MyInfoPage,MyInfoPageTest)

--- src/test/java/Pages/MyInfoPage.java
+++ src/test/java/Pages/MyInfoPage.java
@@ -5,6 +5,7 @@ import Base.BaseTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 public class MyInfoPage extends BasePage
 {
